42f40eca6e4fc0b51c3bd1e117e7793fb65dc993
wlady3190 <wlady_3190@hotmail.com>
2025-01-27 22:05:25 -0500

    fix: fix cors

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -15,7 +15,6 @@ import (
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/cors"
 
-	// "github.com/nxadm/tail/ratelimiter"
 	httpSwagger "github.com/swaggo/http-swagger/v2"
 	"github.com/wlady3190/go-social/docs" //! Para generar documentación de swagger
 	"go.uber.org/zap"
@@ -125,16 +124,7 @@ func (app *application) mount() http.Handler {
 
 	// Basic CORS
 	// for more ideas, see: https://developer.github.com/v3/#cross-origin-resource-sharing
-	r.Use(cors.Handler(cors.Options{
-		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
-		AllowedOrigins: []string{"https://*", "http://*"},
-		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
-		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
-		ExposedHeaders:   []string{"Link"},
-		AllowCredentials: false,
-		MaxAge:           300, // Maximum value not ignored by any of major browsers
-	}))
+
 
 	// A good base middleware stack
 	r.Use(middleware.RequestID)
